9fca7349174352cff5b37f0b9c11fc1c7d696910
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-11-09 13:52:38 +0100

    ✨ Added DrmId

---
+++ src/Drammer.Common.Tests/DrmIdTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Drammer.Common.Tests;
+
+public sealed class DrmIdTests
+{
+    [Theory]
+    [InlineData("drm0001", 1)]
+    [InlineData("drm1", 1)]
+    [InlineData("drm999999", 999999)]
+    [InlineData("drm", null)]
+    [InlineData("", null)]
+    [InlineData("dr999999", null)]
+    [InlineData("asf1", null)]
+    [InlineData(null, null)]
+    public void Parse_ReturnsId(string? input, int? expected)
+    {
+        // act
+        var result = DrmId.Parse(input);
+
+        // assert
+        result.Should().Be(expected);
+    }
+}
---
+++ src/Drammer.Common/DrmId.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Drammer.Common;
+
+/// <summary>
+/// The DRM id.
+/// </summary>
+public static partial class DrmId
+{
+    /// <summary>
+    /// Gets the DRM id.
+    /// </summary>
+    /// <param name="input">
+    /// The input.
+    /// </param>
+    /// <returns>
+    /// The <see cref="int"/>. Null when not found.
+    /// </returns>
+    public static int? Parse(string? input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return null;
+        }
+
+        var sanitized = input.Trim().ToLower();
+
+        var r = DrmRegex();
+        var match = r.Match(sanitized);
+        if (match.Groups.Count > 1 && int.TryParse(match.Groups[1].Value, out var value))
+        {
+            return value;
+        }
+
+        return null;
+    }
+
+    [GeneratedRegex("^drm(\\d+)")]
+    private static partial Regex DrmRegex();
+}
