2a92667e1048963249197a874920a379de951b5c
Marthijn van den Heuvel <marthijn@sidio.nl>
2024-12-27 16:33:35 +0100

    🔥 Removed Mapper classes

--- src/Drammer.Common.Tests/Mapping/DestinationModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Drammer.Common.Tests.Mapping;
-
-public sealed class DestinationModel
-{
-    public string? Name { get; set; }
-}
--- src/Drammer.Common.Tests/Mapping/SourceModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Drammer.Common.Tests.Mapping;
-
-public sealed class SourceModel
-{
-    public string? Name { get; set; }
-}
