74d1237f7563905d7d8d3f23b48afbd12bf6d7d4
Marthijn van den Heuvel <marthijn@sidio.nl>
2025-04-09 09:53:45 +0200

    ✨ Add TruncatedSHA256 class for computing truncated SHA256 hashes

---
+++ src/Drammer.Common.Tests/Cryptography/TruncatedSHA256Tests.cs
@@ -0,0 +1,30 @@
+﻿using Drammer.Common.Cryptography;
+
+namespace Drammer.Common.Tests.Cryptography;
+
+public sealed class TruncatedSHA256Tests
+{
+    [Theory]
+    [InlineData("test", "n4bQgYhMfWWaL+qgxVrQFQ==")]
+    public void ComputeToBase64_ReturnsResult(string input, string expected)
+    {
+        // Act
+        var result = TruncatedSHA256.ComputeToBase64(input);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("test", "9F86D081884C7D659A2FEAA0C55AD015")]
+    public void ComputeToHex_ReturnsResult(string input, string expected)
+    {
+        // Act
+        var result = TruncatedSHA256.ComputeToHex(input);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Should().Be(expected);
+    }
+}
---
+++ src/Drammer.Common/Cryptography/TruncatedSHA256.cs
@@ -0,0 +1,41 @@
+﻿namespace Drammer.Common.Cryptography;
+
+public static class TruncatedSHA256
+{
+    /// <summary>
+    /// Computes the SHA256 hash of the input data and truncates it to 128 bits (16 bytes).
+    /// </summary>
+    /// <param name="data">The data.</param>
+    /// <returns>The hashed data.</returns>
+    public static byte[] Compute(byte[] data)
+    {
+        var hash = System.Security.Cryptography.SHA256.HashData(data);
+        var truncatedHash = new byte[16];
+        Buffer.BlockCopy(hash, 0, truncatedHash, 0, truncatedHash.Length);
+        return truncatedHash;
+    }
+
+    /// <summary>
+    /// Computes the SHA256 hash of the input string and truncates it to 128 bits (16 bytes).
+    /// </summary>
+    /// <param name="data">The data.</param>
+    /// <returns>The hashed data as base 64 string.</returns>
+    public static string ComputeToBase64(string data)
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(data);
+        var hash = Compute(bytes);
+        return Convert.ToBase64String(hash);
+    }
+
+    /// <summary>
+    /// Computes the SHA256 hash of the input string and truncates it to 128 bits (16 bytes).
+    /// </summary>
+    /// <param name="data">The data.</param>
+    /// <returns>The hashed data as hex string.</returns>
+    public static string ComputeToHex(string data)
+    {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(data);
+        var hash = Compute(bytes);
+        return Convert.ToHexString(hash);
+    }
+}
