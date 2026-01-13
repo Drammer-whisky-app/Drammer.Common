81196a3a4d8b3498b17f155f53b7ebc6c29b2a23
Marthijn van den Heuvel <marthijn@sidio.nl>
2026-01-13 08:25:55 +0100

    Remove support for .NET 8.x and 9.x

--- src/Drammer.Common/Extensions/DateTimeExtensions.cs
+++ src/Drammer.Common/Extensions/DateTimeExtensions.cs
@@ -5,60 +5,60 @@
 /// </summary>
 public static class DateTimeExtensions
 {
-    /// <summary>
-    /// Returns the earliest date of two values.
-    /// </summary>
     /// <param name="currentValue">
     /// The current value.
     /// </param>
-    /// <param name="newValue">
-    /// The new value.
-    /// </param>
-    /// <returns>
-    /// The <see cref="DateTime"/>.
-    /// </returns>
-    public static DateTime? GetMinDate(this DateTime? currentValue, DateTime? newValue)
+    extension(DateTime? currentValue)
     {
-        if (newValue == null)
+        /// <summary>
+        /// Returns the earliest date of two values.
+        /// </summary>
+        /// <param name="newValue">
+        /// The new value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="DateTime"/>.
+        /// </returns>
+        public DateTime? GetMinDate(DateTime? newValue)
         {
-            return currentValue;
+            if (newValue == null)
+            {
+                return currentValue;
+            }
+
+            if (currentValue == null)
+            {
+                return newValue;
+            }
+
+            return newValue < currentValue ? newValue : currentValue;
         }
 
-        if (currentValue == null)
+        /// <summary>
+        /// Returns the latest date of two values.
+        /// </summary>
+        /// <param name="newValue">
+        /// The new value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="DateTime"/>.
+        /// </returns>
+        public DateTime? GetMaxDate(DateTime? newValue)
         {
-            return newValue;
-        }
+            if (newValue == null)
+            {
+                return currentValue;
+            }
 
-        return newValue < currentValue ? newValue : currentValue;
+            if (currentValue == null)
+            {
+                return newValue;
+            }
+
+            return newValue > currentValue ? newValue : currentValue;
+        }
     }
-    
-    /// <summary>
-    /// Returns the latest date of two values.
-    /// </summary>
-    /// <param name="currentValue">
-    /// The current value.
-    /// </param>
-    /// <param name="newValue">
-    /// The new value.
-    /// </param>
-    /// <returns>
-    /// The <see cref="DateTime"/>.
-    /// </returns>
-    public static DateTime? GetMaxDate(this DateTime? currentValue, DateTime? newValue)
-    {
-        if (newValue == null)
-        {
-            return currentValue;
-        }
 
-        if (currentValue == null)
-        {
-            return newValue;
-        }
-
-        return newValue > currentValue ? newValue : currentValue;
-    }
-    
     /// <summary>
     /// Change the year of a datetime.
     /// </summary>
